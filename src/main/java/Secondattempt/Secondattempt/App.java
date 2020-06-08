bb7554ca9e171a3a321143939cab50b124467152
mohanjuly1988 <mohanjuly1988@gmail.com>
2020-06-09 00:53:49 +0530

    First commit

---
+++ src/main/java/Secondattempt/Secondattempt/App.java
@@ -0,0 +1,13 @@
+package Secondattempt.Secondattempt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
